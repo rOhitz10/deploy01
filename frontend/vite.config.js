0af45eeae3d7c28d453bdc3bf7feb0e905e318dc
Rohit raj singh <131454955+rOhitz10@users.noreply.github.com>
2024-11-22 21:36:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+ plugins: [react()],
   server: {
-    port:8000
-  }
+    proxy: {
+      '/api': {
+        target: 'https://helpgrow.onrender.com', // Your backend API URL
+        changeOrigin: true,
+        secure: false, // If you are using HTTPS with a self-signed cert
+      },
+    },
+  },
 })
