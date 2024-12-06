2778f1ffd151bb998b05aa30277608792f3d47c0
Rohit Raj Singh <singhrohit1020029@gmail.com>
2024-12-06 16:54:13 +0530

    changes in dashboard referral

--- frontend/src/Dashborad_Components/DashHome.jsx
+++ frontend/src/Dashborad_Components/DashHome.jsx
@@ -35,8 +35,8 @@ const DashHome = () => {
     _id: 0,
   })
   const [receiveCount, setreceiveCount] = useState('');
-  const sponsorId = localStorage.getItem('sponsorId');
-  const textToCopy = `https://helpngrow.onrender.com/r/signup/${sponsorId}`;
+  const sponsorEpin = localStorage.getItem('epin');
+  const textToCopy = `https://helpngrow.onrender.com/r/signup/${sponsorEpin}`;
 
   const handleCopy = async () => {
     try {
